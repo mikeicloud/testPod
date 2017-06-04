f1a96adaa107e667d5f493a5c81253fae3dff890
wuxiaokui <mike.wxk@icloud.com>
2017-06-04 13:24:55 +0800

    person files

---
+++ testPod/Classes/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  testPod
+//
+//  Created by wuxiaokui on 04/06/2017.
+//  Copyright © 2017 wuxiaokui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ testPod/Classes/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  testPod
+//
+//  Created by wuxiaokui on 04/06/2017.
+//  Copyright © 2017 wuxiaokui. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
